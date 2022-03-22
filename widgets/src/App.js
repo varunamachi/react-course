f00624939c06daa78b3a860bcde57492f28ac436
Varuna Amachi <varunamachi@gmail.com>
2022-03-22 22:22:42 +0530

    Adding accordion widget - WIP

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is react?",
+    content: "React is a frontend JS framework",
+  },
+  {
+    title: "Why use react?",
+    content: "Because its nice!",
+  },
+  {
+    title: "How to use react",
+    content: "By building different components",
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items}></Accordion>
+    </div>
+  );
 };
---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const rendered = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return (
+    <div className="ui styled accordion">
+      {rendered}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
+};
+
+export default Accordion;
